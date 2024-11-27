aff7636d86b386e1f256671c7e4839035461b167
ToothlessOS <anthony.13661499167@gmail.com>
2024-11-27 14:00:12 +0800

    New results with 9000 samples

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ visualizer.drawDataset(data)
 
 # Set up the prior distribution and additional data and pass into gibbs sampler
 y = data['log return'].to_numpy()
-mean_samples, tau_samples = modeller.gibbs_sampling(1000, modeller.gamma_sampler(0,0), modeller.normal_sampler(0,0),
+mean_samples, tau_samples = modeller.gibbs_sampling(10000, modeller.gamma_sampler(0,0), modeller.normal_sampler(0,0),
                                                     y, # Additional data
                                                     0.1, 0.1, 0, 0.001) # Prior parameters
 
@@ -18,8 +18,8 @@ visualizer.checkSamplerConvergence(mean_samples, "mean")
 visualizer.checkSamplerConvergence(tau_samples, "tau")
 
 # Pick the from the 100th sample for convergence
-mean_samples = mean_samples[100:]
-tau_samples = tau_samples[100:]
+mean_samples = mean_samples[1000:]
+tau_samples = tau_samples[1000:]
 
 visualizer.checkSamplerACF(mean_samples, "mean ACF")
 visualizer.checkSamplerACF(tau_samples, "tau ACF")
